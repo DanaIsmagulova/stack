34888675c9999bcf19a8d469405e2a89e7320a6e
Dana Ismagulova <diko9716@outlook.com>
2016-02-06 08:02:56 +0500

    stack

---
+++ stack/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labs
+{
+    class Program
+    {
+        static Stack<DirectoryInfo> s = new Stack<DirectoryInfo>();
+        static void Main(string[] args)
+        {
+            string path = Console.ReadLine();
+            DirectoryInfo dir = new DirectoryInfo(path);
+            Search(dir);
+            while (s.Count != 0)
+            {
+                Console.WriteLine("В папке \"" + s.Peek().FullName + "\": " + s.Peek().GetFiles().Length + " Файлов");
+                Console.WriteLine("В папке \"" + s.Peek().FullName + "\": " + s.Peek().GetDirectories().Length + " Папок");
+                s.Pop();
+            }
+
+            Console.ReadKey();
+        }
+
+        static void Search(DirectoryInfo dir)
+        {
+            try
+            {
+                int cnt = dir.GetFiles().Length;
+                s.Push(dir);
+                foreach (DirectoryInfo ndir in dir.GetDirectories())
+                {
+                    Search(ndir);
+                }
+            }
+            catch (Exception err)
+            {
+
+            }
+        }
+
+    }
+}
